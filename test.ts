c770657bc4c33606f4908c66d8797d4322d3c6ab
Roman Valihura <roman.valihura@gmail.com>
2019-11-09 16:05:41 +0200

    remove ts stuff

--- test.ts
+++
@@ -1,4 +0,0 @@
-export const getNixConfigList = (dirPath: string) =>
-  node<NodeJS.ErrnoException, string[]>(done => {
-    readdir(dirPath, done);
-  }).map(files => files.filter(fileName => /.*\.nix/i.test(fileName)));
