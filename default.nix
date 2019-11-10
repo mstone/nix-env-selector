5c6dfe7802947c8582643fc15dcff1af30ba7799
Roman Valihura <roman.valihura@gmail.com>
2019-11-10 11:22:43 +0200

    move toolchain setup to nix instead package json

--- default.nix
+++ default.nix
@@ -2,19 +2,23 @@
 let
   inherit (nixpkgs) pkgs;
 
+  purescriptPackages = import (pkgs.fetchFromGitHub {
+    owner = "justinwoo";
+    repo = "easy-purescript-nix";
+    rev = "aa94aeac3a6ad9b4dfa0e807ad1421097d74f663";
+    sha256 = "1kfhi6rscgf165zg4f1s0fgppygisvc7dppxb93n02rypxfxjirm";
+  }) {
+    inherit pkgs;
+  };
+
   nixPackages = [
     pkgs.nodejs-10_x
     pkgs.yarn
-    pkgs.nodePackages.bower
-    pkgs.nodePackages.pulp
+    purescriptPackages.purs-0_13_4
+    purescriptPackages.spago
   ];
 in
-pkgs.stdenv.mkDerivation {
+pkgs.mkShell {
   name = "vscode-env-selector";
   buildInputs = nixPackages;
-  postInstall =
-    ''
-      yarn install
-      bower install
-    '';
 }
